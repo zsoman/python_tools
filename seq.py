826ffb95d5b68590860eb2a0fb114db055ecf934
root <root@redhat.(none)>
2016-11-28 10:00:54 +0100

    sequence tools Committer: root <root@redhat.(none)> new file:   seq.py

---
+++ seq.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# # Extra excercises for Day 4: Sequences
+
+# ### 4.E1
+# Build indices for the two long texts (data/piszkos_fred.txt, data/utas_es_holdvilag.txt) that lets a user search for words and short phrases.
+
+# In[ ]:
+
+
+
+
+# ### 4.E2
+# Build a program that processes raw text and tries to find words that are parts of a proper name. Test your solution manually on the two novels.
+
+# In[ ]:
+
+
+
+
+# ### 4.E3
+# Process the data in data/ner_data.tsv and use it to evaluate your solution to __4.E3__.
+
+# In[ ]:
+
+
+
